8837f1c932d04341719bc9ca62c6880232f20e21
Sasha Gomes <125107644+sashagomes@users.noreply.github.com>
2023-02-12 06:10:59 +0000

    removed line

--- predatorprey/src/Main.java
+++ predatorprey/src/Main.java
@@ -2,6 +2,5 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("Hello again");
-    System.out.println("Hello once more");
   }
 }
