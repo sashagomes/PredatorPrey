5879adebc9034968d823b9c825fafbfb104d91d9
Sasha Gomes Libova <sashagomeslibova@gmail.com>
2023-02-22 22:34:50 -0800

    timeloop scaffolding

--- predatorprey/src/Animal.java
+++ predatorprey/src/Animal.java
@@ -1,21 +1,44 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Animal {
 
     public int id;
     public int pos_x;
     public int pos_y;
+    public float age;
 
     public Animal(int id, int pos_x, int pos_y) {
         this.id = id;
         this.pos_x = pos_x;
         this.pos_y = pos_y;
+        this.age = 0f;
     }
 
     public void describe(){
         System.out.println("pos_x = " + pos_x + ", pos_y = " + pos_y);
     }
 
-    public void update_position(){
-        System.out.println("updating position of animal " + this.id);
+    public void updateAge(float time){
+        this.age += time;
     }
+
+    public void death(){
+        //if age gets too high
+        //if wolf hunger gets too high
+    }
+
+    public void updatePosition(){
+        //move pos_x right or left or stay (randomly) on the grid
+        this.pos_x += ThreadLocalRandom.current().nextInt(-1,2);
+        //move pos_y up or down or stay (randomly) on the grid
+        this.pos_y += ThreadLocalRandom.current().nextInt(-1,2);
+    }
+
+
+
+    }
+
+
+
 
 }
--- predatorprey/src/Wolf.java
+++ predatorprey/src/Wolf.java
@@ -1,13 +1,25 @@
 public class Wolf extends Animal {
 
     public float vision;
+    public float hunger;
 
     public Wolf(int id, int pos_x, int pos_y, float vision) {
         super(id, pos_x, pos_y);
         this.vision = vision;
+        this.hunger = 0f;
     }
 
-    public void howl(){
-        System.out.println("Auuuu!");
+    public void updateHunger(float time_in_hours) {
+        float delta_hunger_per_hour = 3.4f;
+        // hunger increase by time multiplied by ratio (hunger increase per hour
+        hunger += delta_hunger_per_hour * time_in_hours;
     }
+
+    public void eat_a_rabbit(){
+        float hunger_points_per_rabbit = 25.3f;
+        this.hunger -= hunger_points_per_rabbit;
+    }
+
+
+
 }
